708b9b0396244f38a5decbf2f1e52dcc4037bcb1
LarissonRivero <Larissonrivero2@gmail.com>
2022-12-31 00:38:40 -0300

    Creacion del Grafico con Chart JS

--- assest/js/main.js
+++ assest/js/main.js
@@ -1,35 +1,81 @@
-const url = 'https://mindicador.cl/api/';
-const filterMonedas = ['dolar', 'euro', 'uf', 'utm'];
+const url = 'https://mindicador.cl/api';
+const filterCurrencies = ['dolar', 'euro', 'uf', 'utm'];
+const selectWithCurrencies = document.getElementById('currency');
+const divResult = document.getElementById('result')
 
-const converCase = (str) => {
-  return str.code.charAt(0).toUpperCase() + str.code.slice(1);
-};
+const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
 
 //Obtener Información de la API
-const getMonedas = async () => {
-  const solApi = await fetch(url);
-  const respApi = await solApi.json();
+const getCurrencies = async () => {
+    const reqCurrencies = await fetch(url);
+    const resData = await reqCurrencies.json();
 
-const listMonedas = filterMonedas.map((monedas) => {
-  return{
-    code:respApi[monedas].codigo,
-    value:respApi[monedas].valor,
-  };
-});
-// Mostar Monedas
-const list = document.getElementById('cantidadMonedas');
-  listMonedas.forEach((monedas) => {
-    const opcion = document.createElement('option');
-      opcion.value = monedas.value;
-      opcion.textContent = capitalize(monedas.code);
-      list.appendChild(opcion);
+    const currencyList = filterCurrencies.map((currency) => {
+      return{
+        code:resData[currency].codigo,
+        value:resData[currency].valor,
+      };
+  });
+  // Mostar Monedas
+  currencyList.forEach((localCurrency) => {
+    const option = document.createElement('option');
+      option.value = localCurrency.value;
+      option.text = capitalize(localCurrency.code);
+      selectWithCurrencies.appendChild(option);
   });
 
-console.log(listMonedas);
 };
 
 //Calculo del total a pagar
-const resultadoMonedas = (acound, monedas) => {
+const calcResult = (acound, currency) => {
+  divResult.innerHTML = `$ ${(acound/currency).toFixed(2)}`
+};
 
-}
-resultadoMonedas()
+       //Mostrar Grafico
+  const drawChart = async (currency) => {
+  try {
+    reqChart = await fetch(`${url}/${currency}`);
+    dataChart = await reqChart.json();
+
+    const serieToChart = dataChart.serie.slice(0, 10);
+
+    //grafico
+    const data = {
+      labels: serieToChart.map((item) => item.fecha.substring(0, 10)),
+      datasets: [
+        {
+          label: currency,
+          data: serieToChart.map((item) => item.valor),
+          fill: false,
+          borderColor: 'rgb(75, 192, 192)',
+          tension: 0.1,
+        },
+      ],
+    };
+    const config = {
+      type: 'line',
+      data: data,
+    };
+    const chartDom = document.getElementById('chart');
+    chartDom.classList.remove('d-none');
+    new Chart(chartDom, config);
+  } catch (error) {
+    alert('Error al obetener la Data');
+  }
+};
+
+document.getElementById('btn').addEventListener('click', () => {
+const acoundMonedas = document.getElementById('currency').value;
+  if (acoundMonedas === '') {
+    alert ('Debes Ingresar un Monto')
+      return;
+  }
+    const currencySelected = selectWithCurrencies.value;
+    const codeCurrencySelected = selectWithCurrencies.options[selectWithCurrencies.selectedIndex].text.toLowerCase();
+
+    calcResult(acoundMonedas, currencySelected);
+    drawChart(codeCurrencySelected);
+});
+getCurrencies();
+
+
